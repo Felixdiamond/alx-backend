365e6d44a72fd3e216d67154538301cd240ef38d
Felixdiamond <diamondfelix006@gmail.com>
2023-07-22 11:48:34 -0700

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -19,15 +19,16 @@ def index_range(page: int, page_size: int) -> tuple:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -48,18 +49,24 @@ class Server:
         """Returns a dictionary containing pagination metadata"""
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
-        start_index, end_index = index_range(page, page_size)
 
-        next_page = page + 1 if end_index < len(self.dataset()) else None
-        prev_page = page - 1 if start_index > 0 else None,
+        if page + 1 <= total_pages:
+            next_page = page + 1
+        else:
+            next_page = None
+
+        if page - 1 > 0:
+            prev_page = page - 1
+        else:
+            prev_page = None
 
         hyper_page = {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages,
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
         }
 
         return hyper_page
