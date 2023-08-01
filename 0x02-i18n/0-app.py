644421946404221cd0a9028ecf707c354b4437cd
Felixdiamond <diamondfelix006@gmail.com>
2023-08-01 15:09:42 -0700

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """Index route"""
     return render_template('index.html')
