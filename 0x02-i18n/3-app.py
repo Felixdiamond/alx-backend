c39ce50d1fbfc560768c85b9cdfbcfe96862e93f
Felixdiamond <diamondfelix006@gmail.com>
2023-08-01 16:13:48 -0700

    i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
-"""Flask app with Babel"""
+'''
+    Use Babel to get user locale.
+'''
+
+from flask_babel import Babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext as _
+
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
-class Config:
-    """Config class for Babel"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+class Config(object):
+    '''
+        Babel configuration.
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
 
 
+@app.route('/', methods=['GET'], strict_slashes=False)
+def helloWorld() -> str:
+    '''
+        Render template for Babel usage.
+    '''
+    return render_template('3-index.html')
+
+
 @babel.localeselector
 def get_locale() -> str:
-    """Get best matching locale from request"""
+    '''
+        Get user locale to serve matching translation.
+    '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """Index route"""
-    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
