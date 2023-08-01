3f64121cb52d28e1d97e6ec215593e5206f7037c
Felixdiamond <diamondfelix006@gmail.com>
2023-08-01 15:06:07 -0700

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    """Index route"""
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run()
+
