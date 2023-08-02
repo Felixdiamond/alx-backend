5d4a55472b81bdef07fd2096ace30423e7f62bf3
Felixdiamond <diamondfelix006@gmail.com>
2023-08-01 17:52:13 -0700

    i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -29,7 +29,7 @@ def get_locale() -> str:
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """Index route"""
-    return render_template('index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
